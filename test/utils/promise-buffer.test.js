880e9a21f5c02513445a04ddb954de531230865f
Fredrik Carlsson <me@fc.io>
2016-08-12 13:21:23 +0200

    add test for promise buffer

--- test/utils/promise-buffer.test.js
+++ test/utils/promise-buffer.test.js
@@ -10,12 +10,37 @@ test('buffer.create should return a valid object', (t) => {
   t.end()
 })
 
-
 test('buffer.push should trigger callback', (t) => {
   t.plan(1)
   const buffer = create()
   push(buffer, {
     type: 'run first',
+    callback: t.false,
+  })
+})
+
+test('buffer.push x2 and same type should trigger 2 callbacks', (t) => {
+  t.plan(2)
+  const buffer = create()
+  push(buffer, {
+    type: 'run first',
+    callback: t.false,
+  })
+  push(buffer, {
+    type: 'run first',
+    callback: t.false,
+  })
+})
+
+test('buffer.push x2 and different types should trigger 2 callbacks', (t) => {
+  t.plan(2)
+  const buffer = create()
+  push(buffer, {
+    type: 'run first',
+    callback: t.false,
+  })
+  push(buffer, {
+    type: 'run 2nd',
     callback: t.false,
   })
 })
